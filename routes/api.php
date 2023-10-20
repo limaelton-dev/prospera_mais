84e5af38daaa51f4c11053d78d5355ea7617fcab
Elton Lima <eltonlima.contato@gmail.com>
2023-10-20 05:49:14 -0300

    middleware and auth token is implemented

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ApiWalletController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,9 +21,25 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/wallets', ApiWalletController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::resource('/wallets', ApiWalletController::class);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::post('wallets/{id}/withdraw', [ApiWalletController::class, 'withdraw']);
+    Route::post('wallets/{id}/deposit', [ApiWalletController::class, 'deposit']);
+    Route::post('wallets/{id}/transfer', [ApiWalletController::class, 'transfer']);
 
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('wallets/{id}/withdraw', [ApiWalletController::class, 'withdraw']);
-Route::post('wallets/{id}/deposit', [ApiWalletController::class, 'deposit']);
-Route::post('wallets/{id}/transfer', [ApiWalletController::class, 'transfer']);
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    
+    if(Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+
+    $user = Auth::user();
+    $token = $user->createToken('token');
+
+    return response()->json($token->plainTextToken);
+});
